c7ab109229cb5467276035fb1300ab5eeeabf1c6
mchjmwjw <mchjmwjw@163.com>
2017-02-26 19:23:03 +0800

    项目雏形

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #coding=utf-8
 from flask import Flask
 from flask import request
+from flask import make_response
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,6 +13,12 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, %s!</h1>' % name
+
+@app.route('/cook')
+def cook():
+    response = make_response(u'<h1>这个文件携带了一个cookie</h1>')
+    response.set_cookie('answer', '42')
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
