7662e3457efde1091b5cd601ec26841138b1ae94
mchjmwjw <mchjmwjw@163.com>
2017-02-27 20:39:55 +0800

    项目进一步

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 #coding=utf-8
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 from flask import make_response
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<h1>Hello World!</h1><br>' + u'<p>Your 浏览器 is %s' % user_agent
+    return render_template('index.html')
+    # user_agent = request.headers.get('User-Agent')
+    # return '<h1>Hello World!</h1><br>' + u'<p>Your 浏览器 is %s' % user_agent
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
+    # return '<h1>Hello, %s!</h1>' % name
 
 @app.route('/cook')
 def cook():
@@ -21,4 +27,5 @@ def cook():
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    manager.run()
