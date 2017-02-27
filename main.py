fe3c7ab254a1e6b3dac4359632948b6a7b16b466
WangJiawei <mchjmwjw@163.com>
2017-02-28 01:32:03 +0800

    完善模板，添加favcion, 本地化时间

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from flask import request
 from flask import make_response
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
     # user_agent = request.headers.get('User-Agent')
     # return '<h1>Hello World!</h1><br>' + u'<p>Your 浏览器 is %s' % user_agent
 
@@ -25,6 +28,15 @@ def cook():
     response = make_response(u'<h1>这个文件携带了一个cookie</h1>')
     response.set_cookie('answer', '42')
     return response
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     # app.run(debug=True)
